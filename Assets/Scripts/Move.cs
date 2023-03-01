8629078e30298402303a7511a454d4d4602dffbe
111huan <huang19014@gmail.com>
2023-03-01 18:33:30 +0800

    1

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -4,16 +4,96 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
+    public static string state = "solid";
     Rigidbody rb;
+    float gasSpeed = 5;
+    float zSpeed = 8;
+    bool stop = false;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        rb.velocity = new Vector3(0, 0, 8);
+        rb.velocity = new Vector3(0, 0, zSpeed);
     }
 
     // Update is called once per frame
     void Update()
     {
+        stateCtl();
+        speedCtl();
+    }
+
+    void speedCtl()
+    {
+        rb.velocity = new Vector3(0, rb.velocity.y, zSpeed);
+        if (state == "gas")
+        {
+            gasMove();
+        }
+        if (stop)
+        {
+            rb.velocity = new Vector3(0, 0, 0);
+        }
+    }
+    void stateCtl()
+    {
         
+        if (Input.GetKeyDown("1"))
+        {
+            Debug.Log("solid");
+            state = "solid";
+            rb.useGravity = true;
+        }
+        else if (Input.GetKeyDown("2"))
+        {
+            Debug.Log("liquid");
+            state = "liquid";
+            rb.useGravity = true;
+            stop = false;
+        }
+        else if (Input.GetKeyDown("3"))
+        {
+            Debug.Log("gas");
+            state = "gas";
+            rb.useGravity = false;
+            stop = false;
+        }
+
+        
+    }
+
+    void gasMove()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + gasSpeed * Time.deltaTime, transform.position.z);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y - gasSpeed * Time.deltaTime, transform.position.z);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "wind" && state == "gas")
+        {
+            Debug.Log("------FAIL------");
+        }
+        if (other.gameObject.tag == "thorn" && state == "solid")
+        {
+            Debug.Log("------FAIL------");
+        }
+        if (other.gameObject.tag == "DrainFloor" && state == "liquid")
+        {
+            Debug.Log("------FAIL------");
+        }
+        if (other.gameObject.tag == "success")
+        {
+            Debug.Log("------SUCCESS------");
+        }
+        if(other.gameObject.tag == "drain" && state == "solid")
+        {
+            stop = true;
+        }
     }
 }
